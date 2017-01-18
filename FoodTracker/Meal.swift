ca940cdeb992103e3ae103b9e3521a4a4244c765
Chris Chiang <cchiang1992@gmail.com>
2017-01-17 22:45:06 -0800

    Define data model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,39 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Christopher Chiang on 1/17/17.
+//  Copyright © 2017 Christopher Chiang. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        
+    }
+}
